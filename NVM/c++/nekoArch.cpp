443baf54c20eda0cf20fd4d47668128af42a862e
NekoMimiOfficial <nekomimiofficial@proton.me>
2023-12-09 11:32:52 +0300

    reworking Virtual Machine

--- NVM/c++/nekoArch.cpp
+++ NVM/c++/nekoArch.cpp
@@ -4,12 +4,10 @@
 class Machine
 {
   private:
-    int stack[1024];
+    int mem[1024*8];
     int pc = 0;
     int r0;
     int r1;
-    int rs;
-    int rx;
   public:
 
     enum ID
@@ -20,51 +18,20 @@ class Machine
       JEQU = 70, 
       PRINT = 11, 
       JMP = 32, 
-      INPUT = 50
+      INPUT = 50, 
+      REP = 12
     };
 
-    int pop()
-    {
-      int val = stack[pc];
-      stack[pc--] = 0;
-      return val;
-    }
+    void read()
+    {}
 
-    void push(int x)
-    {
-      stack[++pc] = x;
-      return;
-    }
+    void write()
+    {}
 
-    void jump(int x)
-    {
-      pc = x;
-      return;
-    }
+    void reg_1()
+    {}
 
-    void set0(int x)
-    {
-      r0 = x;
-    }
+    void reg_2()
+    {}
 
-    void set1(int x)
-    {
-      r1 = x;
-    }
-
-    void getx(int x)
-    {
-      rx = x;
-    }
-
-    int halt()
-    {
-      return r0;
-    }
-
-    void print()
-    {
-      std::cout << r0 << "\n";
-      return;
-    }
 };
